2f810a0eb3eda65d20e6b0e1b1f3e9665eb251ca
qianlibin <275532160@qq.com>
2019-02-21 16:15:16 +0800

    商品管理

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -25,6 +25,11 @@
               <el-button slot="append" icon="el-icon-search"></el-button>
             </el-input>
           </div>
+          <div class="flex" style="margin-top: 10px;" v-if="!currentGood.isShelves">
+            <span style="flex: 1;"></span>
+            <el-button type="success" size="small" @click="goodsOnShelvesConfirm">上架</el-button>
+            <el-button type="danger" size="small" @click="delGoods">删除</el-button>
+          </div>
         </div>
       </div>
     </div>
@@ -32,18 +37,20 @@
       <div class="goods-list">
         <div class="goods-item" v-for="(item, index) in allGoodsList" :key="index">
           <el-button :type="item.isShelves?'danger':'success'" :plain="!item.current" :size="currentIndex===index?'':'small'" style="width: 50px; padding: 9px 0; text-align: center;" @click="selectGoods(index)">{{index+1}}</el-button>
+          <i v-if="item.right" class="el-icon-circle-check"></i>
         </div>
         <div class="goods-item">
           <el-button type="primary" :plain="currentIndex!==-1" :size="currentIndex===-1?'':'small'" @click="addNewGoods">{{allGoodsList.length+1}}</el-button>
         </div>
+        <div class="goods-item">
+          <el-button type="primary" @click="addNewGoods">批量添加宝贝</el-button>
+        </div>
       </div>
     </div>
     <div style="margin-top: 10px;">
-      <el-button type="success" size="mini" @click="goodsOnShelves">上架</el-button>
       <el-button size="mini">刷新</el-button>
       <el-button size="mini">前移</el-button>
       <el-button size="mini">后移</el-button>
-      <el-button type="danger" size="mini">删除</el-button>
     </div>
   </div>
 </template>
@@ -105,71 +112,95 @@ export default {
         url: encodeURI(url),
         _: new Date().getTime()
       }
-      let res = {
-        headers: {},
-        model: {
-          imgUrl: 'i4/4205584633/O1CN01YKLBvt1k5xTDWcydu_!!4205584633.jpg',
-          extendVal: { useableItem: 'true', isNew: 'false' },
-          imgList: [
-            '//img.alicdn.com/bao/uploaded/i4/4205584633/O1CN01YKLBvt1k5xTDWcydu_!!4205584633.jpg',
-            '//img.alicdn.com/bao/uploaded/i3/4205584633/O1CN01ozoWva1k5xRuTTcVQ_!!4205584633.jpg',
-            '//img.alicdn.com/bao/uploaded/i4/4205584633/O1CN01LO2L5d1k5xRtLCaCQ_!!4205584633.jpg',
-            '//img.alicdn.com/bao/uploaded/i1/4205584633/O1CN011k5xRd0JLoXObW7_!!4205584633.jpg',
-            '//img.alicdn.com/bao/uploaded/i2/4205584633/O1CN01KCiwxv1k5xRnPJG3T_!!4205584633.jpg'
-          ],
-          height: 0,
-          is11: false,
-          useable: true,
-          width: 0,
-          from: 'taobao',
-          itemPrice: 156.0,
-          itemId: 5804409546973,
-          taokprice: '0.00',
-          itemTitle: '光希 玻尿酸补水初见面膜 补水保湿修护提亮肤色清洁女正品2'
-        },
-        httpStatusCode: 200,
-        msgCode: 'SUCCESS',
-        msgInfo: '成功',
-        success: true
-      }
-      // liveAction(params).then(res => {
-      console.log(res)
-      if (res.success) {
-        let model = res.model
-        let { itemTitle, itemPrice, itemId, imgUrl } = model
-        let count = 0
-        this.allGoodsList.forEach(item => {
-          if (item.itemId === itemId) {
-            count++
+      // let res = {
+      //   headers: {},
+      //   model: {
+      //     imgUrl: 'i4/4205584633/O1CN01YKLBvt1k5xTDWcydu_!!4205584633.jpg',
+      //     extendVal: { useableItem: 'true', isNew: 'false' },
+      //     imgList: [
+      //       '//img.alicdn.com/bao/uploaded/i4/4205584633/O1CN01YKLBvt1k5xTDWcydu_!!4205584633.jpg',
+      //       '//img.alicdn.com/bao/uploaded/i3/4205584633/O1CN01ozoWva1k5xRuTTcVQ_!!4205584633.jpg',
+      //       '//img.alicdn.com/bao/uploaded/i4/4205584633/O1CN01LO2L5d1k5xRtLCaCQ_!!4205584633.jpg',
+      //       '//img.alicdn.com/bao/uploaded/i1/4205584633/O1CN011k5xRd0JLoXObW7_!!4205584633.jpg',
+      //       '//img.alicdn.com/bao/uploaded/i2/4205584633/O1CN01KCiwxv1k5xRnPJG3T_!!4205584633.jpg'
+      //     ],
+      //     height: 0,
+      //     is11: false,
+      //     useable: true,
+      //     width: 0,
+      //     from: 'taobao',
+      //     itemPrice: 156.0,
+      //     itemId: 5804409546973,
+      //     taokprice: '0.00',
+      //     itemTitle: '光希 玻尿酸补水初见面膜 补水保湿修护提亮肤色清洁女正品2'
+      //   },
+      //   httpStatusCode: 200,
+      //   msgCode: 'SUCCESS',
+      //   msgInfo: '成功',
+      //   success: true
+      // }
+      liveAction(params).then(res => {
+        console.log(res)
+        if (res.success) {
+          let model = res.model
+          let { itemTitle, itemPrice, itemId, imgUrl } = model
+          let count = 0
+          this.allGoodsList.forEach(item => {
+            if (item.itemId === itemId) {
+              count++
+            }
+          })
+          if (count > 0) {
+            this.$message.error('宝贝重复添加')
+            return
           }
+          let item = {
+            itemTitle,
+            itemPrice,
+            itemId,
+            // 图片地址
+            imgUrl: baseImgUrl + imgUrl,
+            // 商品链接
+            url: url,
+            // 利益点
+            right: '',
+            // 优惠券链接
+            link: '',
+            isShelves: false,
+            current: true
+          }
+          this.resetGoodsList()
+          this.goodsList.push(item)
+          this.saveGoodsList()
+          this.$nextTick(() => {
+            this.currentIndex = this.allGoodsList.length - 1
+          })
+        } else {
+          this.$message.error(res.msgInfo)
+        }
+      })
+    },
+    // 上架二次确认框
+    goodsOnShelvesConfirm() {
+      let currentGood = this.allGoodsList[this.currentIndex]
+      this.waitUpList = []
+      this.waitUpList.push(currentGood)
+      let count = 0
+      this.waitUpList.forEach(item => {
+        if (item.right === '') {
+          count++
+        }
+      })
+      let msg = `将上架所有${this.waitUpList.length}件宝贝。${count > 0 ? `其中${count}件未设置利益点，` : ''} 是否继续？`
+      this.$confirm(msg, '提示', {
+        confirmButtonText: '是的，立刻上架',
+        cancelButtonText: '不要',
+        type: 'warning'
+      })
+        .then(() => {
+          this.goodsOnShelves()
         })
-        if (count > 0) {
-          this.$message.error('宝贝重复添加')
-          return
-        }
-        let item = {
-          itemTitle,
-          itemPrice,
-          itemId,
-          // 图片地址
-          imgUrl: baseImgUrl + imgUrl,
-          // 商品链接
-          url: url,
-          // 利益点
-          right: '',
-          // 优惠券链接
-          link: '',
-          isShelves: false,
-          current: true
-        }
-        this.resetGoodsList()
-        this.goodsList.push(item)
-        this.saveGoodsList()
-        this.currentIndex = this.goodsList.length - 1
-      } else {
-        this.$message.error(res.msgInfo)
-      }
-      // })
+        .catch(() => {})
     },
     // 上架宝贝
     goodsOnShelves() {
@@ -205,6 +236,24 @@ export default {
         }
         this.saveGoodsList()
       })
+    },
+    // 删除商品
+    delGoods() {
+      this.$confirm('此操作将删除该商品, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          let realIndex = this.currentIndex - this.upGoodsList.length
+          this.goodsList.splice(realIndex, 1)
+          this.saveGoodsList()
+          // this.$message({
+          //   type: 'success',
+          //   message: '删除成功!'
+          // });
+        })
+        .catch(() => {})
     },
     selectGoods(index) {
       this.currentIndex = index
@@ -250,126 +299,126 @@ export default {
         _: new Date().getTime()
       }
       console.log(params)
-      let res = {
-        api: 'mtop.mediaplatform.video.livedetail.itemlist.withpagination',
-        data: {
-          hotList: [],
-          subVideoItemList: {},
-          totalNum: '3',
-          itemList: [
-            {
-              explained: 'false',
-              goodsIndex: '3',
-              goodsList: [
-                {
-                  bulk: 'false',
-                  createTime: '1550726727000',
-                  duplicate: 'false',
-                  extendVal: { isCpc: '0', subTitle: '', customizedItemRights: '号的', defaultRight: '', liveId: 'null', materialName: 'liveItem_486130633' },
-                  favored: 'false',
-                  goodsIndex: '0',
-                  groupNum: '0',
-                  hasDiscount: 'false',
-                  isCpc: '0',
-                  itemH5TaokeUrl: '//h5.m.taobao.com/awp/core/detail.htm?id=566961679422&scm=1007.13381.38597.101200300000000',
-                  itemId: '566961679422',
-                  itemName: '膜法世家周冬雨定制气垫面膜套装21片深层补水保湿提亮面膜女正品',
-                  itemPic: '//gw.alicdn.com/bao/uploaded/i1/368609005/O1CN01T9hPf42GOL4Tr1i5m_!!0-item_pic.jpg',
-                  itemPrice: '129.90',
-                  itemUrl: '//taoke.mdaren.taobao.com/item.htm?itemId=566961679422&accountId=1950250590&bizType=taolive&utparam=%7B%22_tbk%22:%221%22%7D&scm=1007.13381.38597.101200300000000&pg1stepk=ucm:219685085515_1950250590&spm=a2141.8001249',
-                  liveId: '219685085515',
-                  sellerId: '0'
-                }
-              ],
-              sliceNum: '0'
-            },
-            {
-              explained: 'false',
-              goodsIndex: '2',
-              goodsList: [
-                {
-                  bulk: 'false',
-                  createTime: '1550719512000',
-                  duplicate: 'false',
-                  extendVal: { isCpc: '0', subTitle: '三效合一  以黑吸黑  爆款黑面膜', customizedItemRights: '', defaultRight: '三效合一  以黑吸黑  爆款黑面膜', liveId: 'null', materialName: 'liveItem_486204335' },
-                  favored: 'false',
-                  goodsIndex: '0',
-                  groupNum: '0',
-                  hasDiscount: 'false',
-                  isCpc: '0',
-                  itemH5TaokeUrl: '//h5.m.taobao.com/awp/core/detail.htm?id=44359701868&scm=1007.13381.38597.101200300000000',
-                  itemId: '44359701868',
-                  itemName: '膜法世家黑面膜21片清洁补水保湿护肤套装提亮肤色嫩肤面膜女正品',
-                  itemPic: '//gw.alicdn.com/bao/uploaded/i1/368609005/O1CN01nrw4XV2GOL4StvZxe_!!0-item_pic.jpg',
-                  itemPrice: '119.90',
-                  itemUrl: '//taoke.mdaren.taobao.com/item.htm?itemId=44359701868&accountId=1950250590&bizType=taolive&utparam=%7B%22_tbk%22:%221%22%7D&scm=1007.13381.38597.101200300000000&pg1stepk=ucm:219685085515_1950250590&spm=a2141.8001249',
-                  liveId: '219685085515',
-                  sellerId: '0'
-                }
-              ],
-              sliceNum: '0'
-            },
-            {
-              explained: 'false',
-              goodsIndex: '1',
-              goodsList: [
-                {
-                  bulk: 'false',
-                  createTime: '1550716987000',
-                  duplicate: 'false',
-                  extendVal: { isCpc: '0', subTitle: '', customizedItemRights: '', defaultRight: '', liveId: 'null', materialName: 'liveItem_489235333' },
-                  favored: 'false',
-                  goodsIndex: '0',
-                  groupNum: '0',
-                  hasDiscount: 'false',
-                  isCpc: '0',
-                  itemH5TaokeUrl: '//h5.m.taobao.com/awp/core/detail.htm?id=580440954697&scm=1007.13381.38597.101200300000000',
-                  itemId: '580440954697',
-                  itemName: '光希 玻尿酸补水初见面膜 补水保湿修护提亮肤色清洁女正品',
-                  itemPic: '//gw.alicdn.com/bao/uploaded/i4/4205584633/O1CN01YKLBvt1k5xTDWcydu_!!4205584633.jpg',
-                  itemPrice: '156.00',
-                  itemUrl: '//taoke.mdaren.taobao.com/item.htm?itemId=580440954697&accountId=1950250590&bizType=taolive&utparam=%7B%22_tbk%22:%221%22%7D&scm=1007.13381.38597.101200300000000&pg1stepk=ucm:219685085515_1950250590&spm=a2141.8001249',
-                  liveId: '219685085515',
-                  sellerId: '0'
-                }
-              ],
-              sliceNum: '0'
+      // let res = {
+      //   api: 'mtop.mediaplatform.video.livedetail.itemlist.withpagination',
+      //   data: {
+      //     hotList: [],
+      //     subVideoItemList: {},
+      //     totalNum: '3',
+      //     itemList: [
+      //       {
+      //         explained: 'false',
+      //         goodsIndex: '3',
+      //         goodsList: [
+      //           {
+      //             bulk: 'false',
+      //             createTime: '1550726727000',
+      //             duplicate: 'false',
+      //             extendVal: { isCpc: '0', subTitle: '', customizedItemRights: '号的', defaultRight: '', liveId: 'null', materialName: 'liveItem_486130633' },
+      //             favored: 'false',
+      //             goodsIndex: '0',
+      //             groupNum: '0',
+      //             hasDiscount: 'false',
+      //             isCpc: '0',
+      //             itemH5TaokeUrl: '//h5.m.taobao.com/awp/core/detail.htm?id=566961679422&scm=1007.13381.38597.101200300000000',
+      //             itemId: '566961679422',
+      //             itemName: '膜法世家周冬雨定制气垫面膜套装21片深层补水保湿提亮面膜女正品',
+      //             itemPic: '//gw.alicdn.com/bao/uploaded/i1/368609005/O1CN01T9hPf42GOL4Tr1i5m_!!0-item_pic.jpg',
+      //             itemPrice: '129.90',
+      //             itemUrl: '//taoke.mdaren.taobao.com/item.htm?itemId=566961679422&accountId=1950250590&bizType=taolive&utparam=%7B%22_tbk%22:%221%22%7D&scm=1007.13381.38597.101200300000000&pg1stepk=ucm:219685085515_1950250590&spm=a2141.8001249',
+      //             liveId: '219685085515',
+      //             sellerId: '0'
+      //           }
+      //         ],
+      //         sliceNum: '0'
+      //       },
+      //       {
+      //         explained: 'false',
+      //         goodsIndex: '2',
+      //         goodsList: [
+      //           {
+      //             bulk: 'false',
+      //             createTime: '1550719512000',
+      //             duplicate: 'false',
+      //             extendVal: { isCpc: '0', subTitle: '三效合一  以黑吸黑  爆款黑面膜', customizedItemRights: '', defaultRight: '三效合一  以黑吸黑  爆款黑面膜', liveId: 'null', materialName: 'liveItem_486204335' },
+      //             favored: 'false',
+      //             goodsIndex: '0',
+      //             groupNum: '0',
+      //             hasDiscount: 'false',
+      //             isCpc: '0',
+      //             itemH5TaokeUrl: '//h5.m.taobao.com/awp/core/detail.htm?id=44359701868&scm=1007.13381.38597.101200300000000',
+      //             itemId: '44359701868',
+      //             itemName: '膜法世家黑面膜21片清洁补水保湿护肤套装提亮肤色嫩肤面膜女正品',
+      //             itemPic: '//gw.alicdn.com/bao/uploaded/i1/368609005/O1CN01nrw4XV2GOL4StvZxe_!!0-item_pic.jpg',
+      //             itemPrice: '119.90',
+      //             itemUrl: '//taoke.mdaren.taobao.com/item.htm?itemId=44359701868&accountId=1950250590&bizType=taolive&utparam=%7B%22_tbk%22:%221%22%7D&scm=1007.13381.38597.101200300000000&pg1stepk=ucm:219685085515_1950250590&spm=a2141.8001249',
+      //             liveId: '219685085515',
+      //             sellerId: '0'
+      //           }
+      //         ],
+      //         sliceNum: '0'
+      //       },
+      //       {
+      //         explained: 'false',
+      //         goodsIndex: '1',
+      //         goodsList: [
+      //           {
+      //             bulk: 'false',
+      //             createTime: '1550716987000',
+      //             duplicate: 'false',
+      //             extendVal: { isCpc: '0', subTitle: '', customizedItemRights: '', defaultRight: '', liveId: 'null', materialName: 'liveItem_489235333' },
+      //             favored: 'false',
+      //             goodsIndex: '0',
+      //             groupNum: '0',
+      //             hasDiscount: 'false',
+      //             isCpc: '0',
+      //             itemH5TaokeUrl: '//h5.m.taobao.com/awp/core/detail.htm?id=580440954697&scm=1007.13381.38597.101200300000000',
+      //             itemId: '580440954697',
+      //             itemName: '光希 玻尿酸补水初见面膜 补水保湿修护提亮肤色清洁女正品',
+      //             itemPic: '//gw.alicdn.com/bao/uploaded/i4/4205584633/O1CN01YKLBvt1k5xTDWcydu_!!4205584633.jpg',
+      //             itemPrice: '156.00',
+      //             itemUrl: '//taoke.mdaren.taobao.com/item.htm?itemId=580440954697&accountId=1950250590&bizType=taolive&utparam=%7B%22_tbk%22:%221%22%7D&scm=1007.13381.38597.101200300000000&pg1stepk=ucm:219685085515_1950250590&spm=a2141.8001249',
+      //             liveId: '219685085515',
+      //             sellerId: '0'
+      //           }
+      //         ],
+      //         sliceNum: '0'
+      //       }
+      //     ],
+      //     starList: [],
+      //     brandCard: { tmall: 'false' },
+      //     exclusiveIcons: ['//gw.alicdn.com/mt/TB1cUh7b4SYBuNjSsphXXbGvVXa-153-39.png', '//gw.alicdn.com/mt/TB1FKdNeAyWBuNjy0FpXXassXXa-153-39.png']
+      //   },
+      //   ret: ['SUCCESS::调用成功'],
+      //   v: '2.0'
+      // }
+      getUpGoodsList(params).then(res => {
+        console.log(res)
+        if (res.data && res.data.itemList) {
+          this.itemList = res.data.itemList
+          console.log(this.itemList)
+          let list = []
+          this.itemList.forEach(item => {
+            let goods = item.goodsList[0]
+            let { itemName, itemPrice, itemId, itemPic, itemUrl } = goods
+            let goodsItem = {
+              itemTitle: itemName,
+              itemPrice,
+              itemId,
+              url: `https://item.taobao.com/item.htm?id=${itemId}`,
+              imgUrl: itemPic,
+              // 优惠券链接
+              link: '',
+              // 利益点
+              right: goods.extendVal.customizedItemRights,
+              isShelves: true,
+              current: false
             }
-          ],
-          starList: [],
-          brandCard: { tmall: 'false' },
-          exclusiveIcons: ['//gw.alicdn.com/mt/TB1cUh7b4SYBuNjSsphXXbGvVXa-153-39.png', '//gw.alicdn.com/mt/TB1FKdNeAyWBuNjy0FpXXassXXa-153-39.png']
-        },
-        ret: ['SUCCESS::调用成功'],
-        v: '2.0'
-      }
-      // getUpGoodsList(params).then(res => {
-      console.log(res)
-      if (res.data && res.data.itemList) {
-        this.itemList = res.data.itemList
-        console.log(this.itemList)
-        let list = []
-        this.itemList.forEach(item => {
-          let goods = item.goodsList[0]
-          let { itemName, itemPrice, itemId, itemPic, itemUrl } = goods
-          let goodsItem = {
-            itemTitle: itemName,
-            itemPrice,
-            itemId,
-            url: `https://item.taobao.com/item.htm?id=${itemId}`,
-            imgUrl: itemPic,
-            // 优惠券链接
-            link: '',
-            // 利益点
-            right: goods.extendVal.customizedItemRights,
-            isShelves: true,
-            current: false
-          }
-          list.unshift(goodsItem)
-        })
-        this.upGoodsList = list
-      }
-      // })
+            list.unshift(goodsItem)
+          })
+          this.upGoodsList = list
+        }
+      })
     },
     // 点击弹宝贝
     popGoods() {
@@ -404,7 +453,7 @@ export default {
             type: 'success'
           })
         } else {
-          this.$message.error('弹宝贝失败，' + res.msgInfo);
+          this.$message.error('弹宝贝失败，' + res.msgInfo)
         }
       })
     }
@@ -451,6 +500,12 @@ export default {
       height: 34px;
       margin: 5px 5px;
       display: inline-block;
+      position: relative;
+      .el-icon-circle-check{
+        position: absolute; top: 2px; left: 2px;
+        color: #fc6868;
+        font-size: 8px;
+      }
     }
   }
 }
