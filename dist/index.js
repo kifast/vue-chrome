397b2562a8cca15931b0bdd20be362c34fc8ccce
qianlibin <275532160@qq.com>
2019-02-27 11:46:00 +0800

    git配置修改

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+function script_run(url, onSuccess, onError) {
+    var head = document.getElementsByTagName('head')[0],
+        js = document.createElement('script');
+
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('src', url);
+
+    head.appendChild(js);
+
+    js.onload = function () {
+        if (typeof onSuccess === 'function') {
+            onSuccess();
+        }
+    }
+    js.onerror = function () {
+        if (typeof onError === 'function') {
+            onError();
+        }
+    }
+}
+
+var random = new Date().getTime()
+var q = '?r=' + random
+script_run("https://data.goumee.com/template/jimei/static/js/manifest.js" + q,function () {
+    script_run("https://data.goumee.com/template/jimei/static/js/vendor.js" + q,function () {
+        script_run("https://data.goumee.com/template/jimei/static/js/app.js" + q,function () {
+        })
+    })
+})
